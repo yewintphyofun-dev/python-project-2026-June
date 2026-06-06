552bbac3eec3b15e1dc9ade0da8e6089ca3e2627
yewintphyofun-dev <yewintphyofun@gmail.com>
2026-06-06 15:38:08 +0900

    Update files

---
+++ Chapter_02/010primeNumber.py
@@ -0,0 +1,16 @@
+# 2 မှစ၍ user ထည့်ပေးသော ကိန်းအတွင်း ရှိသမျှ သုဒ္ဓကိန်းများထုတ်ပေးခြင်း
+while True:
+    num = int(input("Enter number to find prime numbers under this number:"))
+    if num < 2:
+        print("Please input another number!!")
+    else:
+        if num == 2:
+            print(num)
+        else:
+            for allint in range(2,num+1):
+                for i in range(2,allint):
+                    if allint % i == 0:
+                        break
+                else:
+                    print(allint,end=" ")       
+        break
